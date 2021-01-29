4456a88b025deaf747dc7aaac29d6fcd8aca3113
unknown <storm00530>
2021-01-29 00:44:41 -0600

    error

--- server/config/key.js
+++ server/config/key.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./prod");
+  module.exports = {
+    mongoURI: process.env.MONGO_URI,
+  };
 } else {
   module.exports = require("./dev");
 }
